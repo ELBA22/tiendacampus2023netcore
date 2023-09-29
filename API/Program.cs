f8360a8f59c469d3097a932028ead952027bbae4
ELBA22 <ELBA22@users.noreply.github.com>
2023-09-29 13:09:07 -0500

    feat: :sparkles: TiendaCampusnet
    EntityFramework

---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using API.Extension;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.ConfigureCors();
+//builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices();
+
+builder.Services.AddDbContext<TiendaCampusContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
